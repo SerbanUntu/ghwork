5a362a1eadd61714161a4945aa0ee043113c0d1e
Serban Untu <133024956+SerbanUntu@users.noreply.github.com>
2026-01-06 22:01:32 +0100

    feat: add hashed color for run number
    - The unique color for each run number should help with quickly identifying which run an event is part of.

--- src/main/java/com/example/github_workflow_tool/cli/CLIPrinter.java
+++ src/main/java/com/example/github_workflow_tool/cli/CLIPrinter.java
@@ -7,17 +7,19 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
+import static org.fusesource.jansi.Ansi.*;
+
 /**
  * Handles utilities for string padding and formatting for the events' data
  */
 public class CLIPrinter {
 
-    private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd:HH:mm:ss.SSSX";
+    private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd:HH:mm:ssX";
     private static final String ANSI_REGEX = "\\x1b\\[[0-9;]*[a-zA-Z]";
     private static final int COMMIT_SHA_LENGTH = 7;
     private static final int MAX_NAME_LENGTH = 13;
     private static final String TEXT_OVERFLOW = "...";
-    private static final int LINE_LENGTH = 110;
+    private static final int LINE_LENGTH = 120;
 
     //map run to id and vice-versa
 
@@ -28,7 +30,26 @@ public class CLIPrinter {
     }
 
     public String formatRunId(long runId) {
-        return "Run " + String.format("%03d$", runId % 1000);
+        long hash = UUID.nameUUIDFromBytes(((Long) runId).toString().getBytes()).getMostSignificantBits();
+        int color = (int) (hash % (1 << 24));
+        int r = color >> 16;
+        int g = (color >> 8) % 256;
+        int b = color % 256;
+
+        float average = (r + g + b) / 3.0f;
+        float normalizationRatio = 128 / average;
+        r = Math.min((int) (r * normalizationRatio), 255);
+        g = Math.min((int) (g * normalizationRatio), 255);
+        b = Math.min((int) (b * normalizationRatio), 255);
+
+        return ansi()
+                .a("Run ")
+                .fgRgb(r, g, b)
+                .a(String.format("%03d", runId % 1000))
+                .fgDefault()
+                .a('$')
+                .reset()
+                .toString();
     }
 
     public String formatTag(String tag, int length) {
